2b4f88a0dd3d652c36883801556c7c52ee5f83ff
calebs2500 <calebs2500@gmail.com>
2015-09-27 21:16:16 +1000

    update for dev v2

--- v2/js/header.js
+++ v2/js/header.js
@@ -27,3 +27,18 @@ if (pageID == "downloads") {
 if (pageID == "hosting") {
 	document.getElementById("buttonHosting").className = "selectedPage";
 }
+
+window.onload = setTimeout(loadFooter);
+
+function loadFooter() {
+	document.getElementById("pageFooter").innerHTML = "<p>&copy VultureSMP, 2015 | Website design, code and media property of <a href='mailto:contact@vulturesmp.com?subject=Enquiry%20from%20website' title='Mail VultureSMP'>VultureSMP</a> | Designed and written by <a href='mailto:calebs2500@gmail.com?subject=Enquiry%20from%20vulturesmp.com' title='Mail Caleb Stewart'>Caleb Stewart</a></p>";
+}
+
+window.onload = setTimeout(calcFooter);
+var footerRecalc;
+
+function calcFooter() {
+	var footerHeight = document.getElementById("pageFooter").clientHeight;
+	document.getElementById("content").style.marginBottom = 20 + footerHeight + "px";
+	footerRecalc = setTimeout(calcFooter,100);
+}
